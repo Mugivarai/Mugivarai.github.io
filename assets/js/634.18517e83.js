8b964c262c26a24edba10220efb05a4be6fffaa4
Mugivarai <102305866+Mugivarai@users.noreply.github.com>
2025-04-13 22:28:29 +0300

    First commit

---
+++ assets/js/634.18517e83.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkWedding"]=self["webpackChunkWedding"]||[]).push([[634],{3634:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
